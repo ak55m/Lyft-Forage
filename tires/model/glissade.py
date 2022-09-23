840f3ceb427baaca5d73f40622e0a1dede73ac9c
Akeem Mohammed <60115485+ak55m@users.noreply.github.com>
2022-09-23 13:17:11 -0500

    lyft

---
+++ tires/model/glissade.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from tires.all_tires import Tires
+
+
+class Glissade(Tires):
+    
+    #this gives confirmation that one or more tire needs to be serviced
+    def tire_needs_service(self):
+        
+        service_threshold_date = self.last_service_date + datetime.today().date()
+
+        if service_threshold_date < datetime.today().date() and self.tire_should_be_serviced():
+            return True
+        else:
+            return False
