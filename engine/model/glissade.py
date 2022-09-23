9612f02a4f693d81d59009e37f5c9dafe25cddfc
Akeem Mohammed <60115485+ak55m@users.noreply.github.com>
2022-09-23 13:14:47 -0500

    Add files via upload

---
+++ engine/model/glissade.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from engine.willoughby_engine import WilloughbyEngine
+
+
+class Glissade(WilloughbyEngine):
+    
+     #this gives confirmation that engine needs to be serviced
+    def engine_needs_service(self):
+
+        service_threshold_date = self.last_service_date
+        if service_threshold_date < datetime.today().date() and self.engine_should_be_serviced():
+            return True
+        else:
+            return False
